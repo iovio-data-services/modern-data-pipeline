0bf8e2d33f1701ebba6c34f1c14b81a8e3b103aa
Herminio Vazquez <herminio.vazquez@iovio.com>
2021-05-13 16:31:11 +0200

    Initial setup with SQL Server

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # modern-data-pipeline
 A collection of architecture templates for ephemeral and on-demand computation of data pipelines 
+
+
+## Installation of MS-SQL Server Database
+```
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=yourStrong(!)Password' -e 'MSSQL_PID=Express' -p 1433:1433 -d --name sql1 mcr.microsoft.com/mssql/server:2017-latest-ubuntu
+```
+
+## Load Bike Shop Information
---
+++ src/solids.py
@@ -0,0 +1,80 @@
+# start-snippet
+from pathlib import Path
+
+from dagster import (
+    ModeDefinition,
+    make_python_type_usable_as_dagster_type,
+    pipeline,
+    repository,
+    solid,
+)
+from dagster.core.definitions.no_step_launcher import no_step_launcher
+from dagster_aws.emr import emr_pyspark_step_launcher
+from dagster_aws.s3 import s3_pickle_io_manager, s3_resource
+from dagster_pyspark import DataFrame as DagsterPySparkDataFrame
+from dagster_pyspark import pyspark_resource
+from pyspark.sql import DataFrame, Row
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
+
+# Make pyspark.sql.DataFrame map to dagster_pyspark.DataFrame
+make_python_type_usable_as_dagster_type(python_type=DataFrame, dagster_type=DagsterPySparkDataFrame)
+
+
+@solid(required_resource_keys={"pyspark", "pyspark_step_launcher"})
+def make_people(context) -> DataFrame:
+    schema = StructType([StructField("name", StringType()), StructField("age", IntegerType())])
+    rows = [Row(name="Thom", age=51), Row(name="Jonny", age=48), Row(name="Nigel", age=49)]
+    return context.resources.pyspark.spark_session.createDataFrame(rows, schema)
+
+
+@solid(required_resource_keys={"pyspark_step_launcher"})
+def filter_over_50(_, people: DataFrame) -> DataFrame:
+    return people.filter(people["age"] > 50)
+
+
+@solid(required_resource_keys={"pyspark_step_launcher"})
+def count_people(_, people: DataFrame) -> int:
+    return people.count()
+
+
+emr_mode = ModeDefinition(
+    name="emr",
+    resource_defs={
+        "pyspark_step_launcher": emr_pyspark_step_launcher.configured(
+            {
+                "cluster_id": {"env": "EMR_CLUSTER_ID"},
+                "local_pipeline_package_path": str(Path(__file__).parent),
+                "deploy_local_pipeline_package": True,
+                "region_name": "us-west-1",
+                "staging_bucket": "my_staging_bucket",
+                "wait_for_logs": True,
+            }
+        ),
+        "pyspark": pyspark_resource.configured({"spark_conf": {"spark.executor.memory": "2g"}}),
+        "s3": s3_resource,
+        "io_manager": s3_pickle_io_manager.configured(
+            {"s3_bucket": "my_staging_bucket", "s3_prefix": "simple-pyspark"}
+        ),
+    },
+)
+
+local_mode = ModeDefinition(
+    name="local",
+    resource_defs={
+        "pyspark_step_launcher": no_step_launcher,
+        "pyspark": pyspark_resource.configured({"spark_conf": {"spark.default.parallelism": 1}}),
+    },
+)
+
+
+@pipeline(mode_defs=[emr_mode, local_mode])
+def my_pipeline():
+    count_people(filter_over_50(make_people()))
+
+
+# end-snippet
+
+
+@repository
+def emr_pyspark_example():
+    return [my_pipeline]
